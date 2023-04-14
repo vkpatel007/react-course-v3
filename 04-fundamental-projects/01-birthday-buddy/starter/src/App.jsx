525a3e31bf55b4ee03cb36e69b1d5b3439b8f56c
vkpatel007 <vkpatel007@gmail.com>
2023-04-14 01:08:41 -0400

    yes yes

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+import data from './data'
+import List from './List';
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPerson] = useState(data)
+
+  return (
+
+    <main>
+      <section className='container'>
+        <h3>There are {people.length} people's birthdays</h3>
+        <List people={people} />
+        <button type='button' className='btn btn-block' onClick={() => setPerson([])}>Remove All</button>
+      </section>
+    </main>
+  )
 };
 export default App;
